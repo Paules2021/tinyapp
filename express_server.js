66ee616d4b45a0846b62711beb919cf1cb5d6362
Paules2021 <P.elishaee@gmail.com>
2022-01-25 00:16:31 +0000

    final_template

--- express_server.js
+++ express_server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8081; // default port 8080
 
-app.set('view engin', 'ejs')
+app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-  
+
 };
 
 app.get("/urls", (req, res) => {
